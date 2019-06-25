f9645cd93540181c0ee0245a0e53a5b8ea1e8b14
nayif03 <naif.nayoul@yahoo.com>
2019-06-25 10:14:49 +0200

    modify Readme and create a function in new js file

---
+++ messaging.js
@@ -0,0 +1,9 @@
+const showHelp = () => {
+    // handle input
+    // no input to see here 
+    const helpText = "welcome to my app"
+    // print output
+    console.log(helpText)
+}
+
+showHelp()
